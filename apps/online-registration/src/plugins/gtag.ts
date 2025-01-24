87c3c4b4162d827b0495fc190892084db1833f77
Abdul Ojulari <abdulkabirojulari@gmail.com>
2025-01-24 10:46:30 -0700

    change gtag plugin

--- apps/online-registration/src/plugins/gtag.ts
+++ apps/online-registration/src/plugins/gtag.ts
@@ -1,9 +1,10 @@
 import VueGtag from 'vue-gtag-next'
 
 export default defineNuxtPlugin((nuxtApp) => {
+  const  gtagId  = useRuntimeConfig().public;
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: 'G-XXXXXXXXXX' // Replace with your GA4 measurement ID
+      id: gtagId.gtagId
     },
     isEnabled: true,
     useDebugger: process.env.NODE_ENV !== 'production'
