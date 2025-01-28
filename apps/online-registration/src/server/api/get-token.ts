0d5ba853594f940815534250b76c7a69adf40e5a
Abdul Ojulari <abdulkabirojulari@gmail.com>
2025-01-28 14:17:06 -0700

    another approach

--- apps/online-registration/src/server/api/get-token.ts
+++ apps/online-registration/src/server/api/get-token.ts
@@ -9,20 +9,17 @@ import {
 
 export default defineEventHandler(async (event: H3Event<EventHandlerRequest>) => {
     const config  = await useRuntimeConfig(event);
-    const body = await readBody(event);
+    //const body = await readBody(event);
      // Create form data string directly
-   // Create query parameters
-   const params = new URLSearchParams({
-    'grant_type': 'client_credentials',
-    'client_id': config.private.client_id,
-    'client_secret': config.private.client_secret,
-});
-
-// Append query parameters to the base URL
-const baseUrl = config.public.NODE_ENV === 'development' ? config.public.tokenUrl : config.private.tokenUrl;
-const url = `${baseUrl}?${params.toString()}`;
+    const formData = new URLSearchParams({
+        grant_type: 'client_credentials',
+        client_id: `${config.private.client_id}`,
+        client_secret: `${config.private.client_secret}`,
+    }).toString();
+    const url = config.public.NODE_ENV === 'development' ? config.public.tokenUrl : config.private.tokenUrl ;
+    
     try {
-        const response = await $fetch(url,{
+        const response = await fetch(url,{
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -31,12 +28,13 @@ const url = `${baseUrl}?${params.toString()}`;
             body: formData
         });
 
-        setCookie(event, 'access_token', response.access_token, {
+        const data = await response.json();
+        setCookie(event, 'access_token', data.access_token, {
             path: '/',
-            maxAge: response.expires_in || 3600 // Use token expiration or default to 1 hour
+            maxAge: data.expires_in || 3600 // Use token expiration or default to 1 hour
         });
 
-        return response.access_token;
+        return data.access_token;
     }
     catch (error) {
         throw createError({
